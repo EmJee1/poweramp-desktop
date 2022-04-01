e96eac04f4afd6189b3224d36ce6141a6968e878
EmJee <67376694+EmJee1@users.noreply.github.com>
2022-04-01 12:57:39 +0200

    Add basic datastore for persistent data

---
+++ src/db/connection.ts
@@ -0,0 +1,15 @@
+import Datastore from 'nedb-promises';
+import { app } from 'electron';
+import path from 'path';
+
+const productionDatabasePath = path.join(app.getPath('userData'), '/data');
+const getDatabasePath = (name: string) =>
+  process.env.NODE_ENV === 'production'
+    ? path.join(productionDatabasePath, `/${name}.db`)
+    : path.join(process.cwd(), `/src/db/data/${name}.db`);
+
+const db = {
+  tracks: Datastore.create(getDatabasePath('tracks')),
+};
+
+export default db;
