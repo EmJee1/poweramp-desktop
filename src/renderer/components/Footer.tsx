a04a366470edcb89c10d89894b62bd3cb8c64c9e
EmJee <mart-janroeleveld@outlook.com>
2022-04-28 19:29:47 +0200

    Footer fixed height

--- src/renderer/components/Footer.tsx
+++ src/renderer/components/Footer.tsx
@@ -4,8 +4,8 @@ import FooterGlobalControls from './FooterGlobalControls';
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-white p-2">
-      <div className="flex items-center justify-between">
+    <div className="fixed bottom-0 left-0 right-0 h-20 bg-white p-2">
+      <div className="flex h-full items-center justify-between">
         <FooterTrackMetadata />
         <FooterTrackControls />
         <FooterGlobalControls />
